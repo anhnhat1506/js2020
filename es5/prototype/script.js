9beee2f30175cbfd202136459334d5c5f1699134
anhnhat1506 <nhat15061992@gmail.com>
2020-11-02 16:16:31 +0700

    constructor and prototype

--- es5/prototype/script.js
+++ es5/prototype/script.js
@@ -2,12 +2,10 @@ function Book(title, author, year) {
     this.title = title;
     this.author = author;
     this.year = year;
-
-
-    this.getSumary = function() {
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    }
 }
+Book.prototype.getSumary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+};
 const book1 = new Book('Book One', 'John Doe', '2013');
 const book2 = new Book('Book Two', 'Revival', '2019');
 console.log(book1.getSumary());
